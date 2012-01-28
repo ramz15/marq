0d5b768f44ac8cd0e0d3992b5c72c9ea15c020ae
Karl Gusner <karlgusner@gmail.com>
2012-01-28 01:58:54 -0500

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marqueed::Application.config.secret_token = '5a9f00efb006dbde862dcea6eff239de3d76ee95f5d55c53f07d19d5d8522ac4bce84ce1ce018e7e6fc73117bb98ebf2e45f45acaca8c5990d9bb3a345033dd0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Marqueed::Application.config.session_store :cookie_store, key: '_marqueed_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Marqueed::Application.config.session_store :active_record_store
